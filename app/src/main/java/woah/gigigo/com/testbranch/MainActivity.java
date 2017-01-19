a77e966df0d9daabc276fea36f393e654f759202
Alberto Sainz Vegas <nuborisar@gmail.com>
2017-01-19 18:42:56 +0100

    multiwindow y proceso intent like woah develop

--- app/src/main/java/woah/gigigo/com/testbranch/MainActivity.java
+++ app/src/main/java/woah/gigigo/com/testbranch/MainActivity.java
@@ -1,6 +1,7 @@
 package woah.gigigo.com.testbranch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,23 +15,26 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
   }
 
   //region branch8 (if the activity is fragment activity check install guide)
-  @Override
-  public void onStart() {
+  @Override public void onStart() {
     super.onStart();
-     Branch branch = Branch.getInstance();
+    Branch branch = Branch.getInstance();
 
-    branch.initSession(new Branch.BranchReferralInitListener(){
-      @Override
-      public void onInitFinished(JSONObject referringParams, BranchError error) {
+    branch.initSession(new Branch.BranchReferralInitListener() {
+      @Override public void onInitFinished(JSONObject referringParams, BranchError error) {
         if (error == null) {
           // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
           // params will be empty if no data found
           // ... insert custom logic here ...
-          Toast.makeText(MainActivity.this,"ReferringParams: "+ referringParams.toString(),Toast.LENGTH_LONG).show();
+          Toast.makeText(MainActivity.this, "ReferringParams: " + referringParams.toString(),
+              Toast.LENGTH_LONG).show();
 
+          //al siguiete se le puede llamar en cualkier momento, depende del intent no de branch
+          Toast.makeText(MainActivity.this, "Deeplink: " + getDeepLinking(), Toast.LENGTH_LONG).show();
         } else {
           Log.i("MyApp", error.getMessage());
         }
@@ -38,10 +42,26 @@ public class MainActivity extends AppCompatActivity {
     }, this.getIntent().getData(), this);
   }
 
-  @Override
-  public void onNewIntent(Intent intent) {
+  @Override public void onNewIntent(Intent intent) {
     this.setIntent(intent);
   }
 
+  private static final String INTENT_FILTER_VIEW = "android.intent.action.VIEW";
 
+  private String getDeepLinking() {
+    Intent intent = getIntent();
+    String action = intent.getAction();
+    Uri data = intent.getData();
+
+    if ((action != null && action.equals(INTENT_FILTER_VIEW)) && data != null) {
+      String host = data.getHost();
+      String path = data.getPath();
+
+      if (!host.isEmpty() && !path.isEmpty()) {
+        return (host + "|" + path); //asv aqui llamais al presenter para q haga el deeplink ^_^
+      }
+    }
+    //fixme la primera vez es VIEW
+    return "so fuck =_= | 0_o";
+  }
 }
